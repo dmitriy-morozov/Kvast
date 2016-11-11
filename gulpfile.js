83cda822c911c431267cca4eebe3fadea10a89df
dmitriy-morozov <dimokin777@mail.ru>
2016-11-11 23:51:59 +0200

    src

---
+++ gulpfile.js
@@ -0,0 +1,102 @@
+"use strict";
+
+// подключаем компоненты gulp
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var browserSync = require('browser-sync').create();
+var run = require("run-sequence");
+var del = require("del");
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+
+
+gulp.task("style", function() {
+  gulp.src("css/style.css")
+    .pipe(plumber())
+    .pipe(postcss([
+      autoprefixer({browsers: [
+        "last 1 version",
+        "last 2 Chrome versions",
+        "last 2 Firefox versions",
+        "last 2 Opera versions",
+        "last 2 Edge versions"
+      ]}),
+      mqpacker({
+          sort: true
+      })
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
+	.pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task("images", function() {
+    return gulp.src("build/img/**/*.{png,jpg,gif}")
+      .pipe(imagemin([
+        imagemin.optipng({optimizationLevel: 3}),
+        imagemin.jpegtran({progressive: true}),
+    ]))
+      .pipe(gulp.dest("build/img"));
+});
+
+gulp.task('scripts', function (cb) {
+  pump([
+        gulp.src('build/js/**/*.js'),
+        uglify(),
+        gulp.dest('build/js')
+    ],
+    cb
+  );
+});
+
+
+gulp.task("server", function() {
+    browserSync.init({
+        server: "build",
+		notify: false,
+		open: true
+    });
+
+  gulp.watch("css/**/*.{css,less}", ["style"]);
+  gulp.watch("*.html").on("change", browserSync.reload);
+});
+
+
+gulp.task("copy", function() {
+    return gulp.src([
+        "fonts/**/*.{woff,woff2}",
+        "img/**",
+        "js/**",
+        "*.html"
+    ], {
+        base: "."
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+    return del("build");
+});
+
+gulp.task("build", function(fn) {
+    run(
+      "clean",
+      "copy",
+      "style",
+	  "scripts",
+      "images",
+      fn
+    );
+});
+
+
+
+
